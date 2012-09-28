16e9b7cd5ac0cdcf73ecfa4a097877498a29209a
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-09-28 13:54:49 -0400

    running out of time… result keeps giving me an error so I took it out for now

--- Week 1/UIDemo/src/com/janellesalisbury/uidemo/MainActivity.java
+++ Week 1/UIDemo/src/com/janellesalisbury/uidemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.janellesalisbury.uidemo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
 	
 	LinearLayout ll;
 	LinearLayout.LayoutParams lp;
+	EditText et;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,17 +25,43 @@ public class MainActivity extends Activity {
         ll.setLayoutParams(lp);
         
         TextView tv = new TextView(this);
-        tv.setText("This is my project.");
+        //tv.setText(getString(R.string.quarter)+","+getString(R.string.dime)+","+getString(R.string.nickel)+","+getString(R.string.penny));
+        tv.setText("Enter the number of dollars to convert to coins!");
         
         ll.addView(tv);
         
-        EditText et = new EditText(this);
-        et.setHint("Please enter info here");
+        et = new EditText(this);
+        et.setHint("Please enter dollars here");
         //ll.addView(et);
         
         Button b = new Button(this);
-        b.setText("Enter");
+        b.setText("Convert");
         //ll.addView(b);
+        b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int quarter = getResources().getInteger(R.integer.quarter);
+				int dime = getResources().getInteger(R.integer.dime);
+				int nickel = getResources().getInteger(R.integer.nickel);
+				int penny = getResources().getInteger(R.integer.penny);
+				
+				
+				int entry = Integer.parseInt (et.getText().toString());
+				
+				int numQ = (100/quarter)*entry;
+				int numD = (100/dime)*entry;
+				int numN = (100/nickel)*entry;
+				int numP = (100/penny)*entry;
+				
+		
+				
+				
+				
+						
+				
+			}
+		});
         
         LinearLayout form = new LinearLayout(this);
         form.setOrientation (LinearLayout.HORIZONTAL);
@@ -44,6 +72,7 @@ public class MainActivity extends Activity {
         
         ll.addView(form);        
         
+  
         
         setContentView(ll);
         
