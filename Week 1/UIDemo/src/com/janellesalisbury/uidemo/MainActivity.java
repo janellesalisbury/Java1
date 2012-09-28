4cf031b318934b5115d8a9016c17c24a0b9cabb9
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-09-28 12:18:41 -0400

    adding text view to ll

--- Week 1/UIDemo/src/com/janellesalisbury/uidemo/MainActivity.java
+++ Week 1/UIDemo/src/com/janellesalisbury/uidemo/MainActivity.java
@@ -3,13 +3,32 @@ package com.janellesalisbury.uidemo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	LinearLayout ll;
+	LinearLayout.LayoutParams lp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(lp);
+        
+        TextView tv = new TextView(this);
+        tv.setText("This is my project.");
+        
+        ll.addView(tv);
+        
+        setContentView(ll);
+        
+        
+        
     }
 
     @Override
