df3e8c6a69e9df3ee38072b21af18497f367a886
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-02 17:15:31 -0400

    starting to add in elements such as text entry and the handler

--- Week 2/SalisburyJanelle_JavaWeek2/src/com/salisburyjanelle_javaweek2/MainActivity.java
+++ Week 2/SalisburyJanelle_JavaWeek2/src/com/salisburyjanelle_javaweek2/MainActivity.java
@@ -5,7 +5,10 @@ package com.salisburyjanelle_javaweek2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,6 +16,9 @@ public class MainActivity extends Activity {
 	LinearLayout ll;
 	LinearLayout.LayoutParams lp;
 	TextView mainHeader;
+	TextView foodItems;
+	EditText et;
+	int itemCount;
 	
 	
 	
@@ -20,6 +26,48 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+       ll = new LinearLayout (this);
+       ll.setOrientation (LinearLayout.VERTICAL);
+       lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+       ll.setLayoutParams(lp);
+       
+        
+        
+        //set up header
+        mainHeader = new TextView(this);
+        mainHeader.setText("Grocery Buddy");
+        mainHeader.setBackgroundColor(Color.LTGRAY);
+        mainHeader.setTextColor(Color.BLACK);
+        
+        foodItems = new TextView(this);
+        String foods[] = {getString(R.string.item1), getString(R.string.item2), getString(R.string.item3)};
+        itemCount = foods.length;
+        
+        et = new EditText(this);
+        et.setHint("Enter food you wish to purchase here");
+        
+        Button b = new Button(this);
+        b.setText("Add to List");
+        
+        
+        
+       
+       
+        
+        
+      
+        ll.addView(mainHeader);
+        
+        setContentView(ll);
+        
+        
+        
+      
+       
+        
+        
+        
         
         
         
