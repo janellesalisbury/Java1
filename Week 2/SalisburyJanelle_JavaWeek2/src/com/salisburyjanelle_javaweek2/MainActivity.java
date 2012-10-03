d517fcb2dcc774d75e835612525e01cf258f2c08
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-02 20:17:14 -0400

    Becoming very frustrated… just don't understand… got a package in for linear layout

--- Week 2/SalisburyJanelle_JavaWeek2/src/com/salisburyjanelle_javaweek2/MainActivity.java
+++ Week 2/SalisburyJanelle_JavaWeek2/src/com/salisburyjanelle_javaweek2/MainActivity.java
@@ -3,24 +3,18 @@ package com.salisburyjanelle_javaweek2;
 //Janelle Salisbury 
 //Grocery List Application
 
+import com.salisburyjanelle.lib.TextForms;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.graphics.Color;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 	LinearLayout ll;
 	LinearLayout.LayoutParams lp;
-	TextView mainHeader;
-	TextView foodItems;
-	EditText et;
-	int itemCount;
-	TextView secondHeader;
+
 	
 	
 	
@@ -29,73 +23,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-       ll = new LinearLayout (this);
-       ll.setOrientation (LinearLayout.VERTICAL);
-       lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-       ll.setLayoutParams(lp);
-       
-        
-        
-        //set up header
-        mainHeader = new TextView(this);
-        mainHeader.setText("Grocery Buddy");
-        mainHeader.setBackgroundColor(Color.LTGRAY);
-        mainHeader.setTextColor(Color.BLACK);
-        
-        foodItems = new TextView(this);
-        String foods[] = {getString(R.string.item1), getString(R.string.item2), getString(R.string.item3), getString(R.string.item4)};
-        itemCount = foods.length;
-        for (int i=0; i<itemCount; i++){
-        foodItems.append((foods[i] + "\n'"));
-        
-        }
-        et = new EditText(this);
-        et.setHint("Enter food you wish to purchase here");
-        
-        Button b = new Button(this);
-        b.setText("Add to List");
-        
-        secondHeader = new TextView(this);
-        secondHeader.setText("Coupon Deals");
-        secondHeader.setTextColor(Color.BLACK);
-        secondHeader.setBackgroundColor(Color.MAGENTA);
-        
-        b.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String entry = et.getText().toString();
-				foodItems.append(entry + "\r\n");
-				
-				
-			}
-		});
-        
-        ll.addView(mainHeader);
-        ll.addView(foodItems);
-        //ll.addView(et);
-        //ll.addView(b);
-       
-        LinearLayout form = new LinearLayout(this);
-        form.setOrientation(LinearLayout.VERTICAL);
-        
-        form.setLayoutParams(lp);
-        form.addView(et);
-        form.addView(b);
-        form.addView(secondHeader);
-        
-        
-         ll.addView(form);
-        
+        LinearLayout ll = new LinearLayout(this);
+        LinearLayout formEntry = TextForms.singleEntryWithButton(this, "Add item you wish to purchase", "Add Item");
+        ll.addView(formEntry);
         setContentView(ll);
         
         
-        
-      
        
-        
-        
-        
         
         
         
