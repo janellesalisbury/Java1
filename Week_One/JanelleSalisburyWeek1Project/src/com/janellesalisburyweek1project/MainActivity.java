fad03f8d387e4f91f8ca09e71813e4dbe2bf3817
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-22 19:34:16 -0400

    Starting Linear Layout

--- Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
+++ Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
@@ -3,13 +3,17 @@ package com.janellesalisburyweek1project;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+	
+	LinearLayout ll;
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
     }
 
     @Override
