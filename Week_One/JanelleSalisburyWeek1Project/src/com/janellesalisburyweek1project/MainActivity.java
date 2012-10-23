9b88899be93e3ec15e607ff1607a33ca34e779b1
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-23 16:46:15 -0400

    adding header, list of parts, title header

--- Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
+++ Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends Activity {
 	LinearLayout.LayoutParams lp;
 	TextView header;
 	TextView firstTV;
+	TextView secondTV;
+	TextView thirdTV;
 	EditText et;
 	int itemCount;
 	int itemCount2;
@@ -28,12 +30,22 @@ public class MainActivity extends Activity {
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         ll.setLayoutParams(lp);
         
-        //set up header and textView
+        //set up title header and textView
         header = new TextView(this);
         header.setText("Restoration Rolodex");
         header.setBackgroundColor(Color.RED);
         header.setTextColor(Color.WHITE);
         ll.addView(header);
+        
+        firstTV = new TextView(this);
+        
+        //Loop of parts from resources
+        String [] parts = {getString(R.string.item1), getString(R.string.item2), getString(R.string.item3), getString(R.string.item4), getString(R.string.item5)};
+        itemCount = parts.length;
+        for(int i=0; i < itemCount; i++){
+        	firstTV.append((parts[i] + "\n"));
+        }
+        ll.addView(firstTV);
         
         
         setContentView (ll);
