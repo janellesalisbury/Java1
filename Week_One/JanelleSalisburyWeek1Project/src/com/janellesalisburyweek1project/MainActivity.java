f5b85f89b9bed59a3d9e428fc9ffb23ab26f58da
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-23 16:57:42 -0400

    main title header, section one header, section items list

--- Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
+++ Week_One/JanelleSalisburyWeek1Project/src/com/janellesalisburyweek1project/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends Activity {
 	LinearLayout ll;
 	LinearLayout.LayoutParams lp;
 	TextView header;
-	TextView firstTV;
-	TextView secondTV;
-	TextView thirdTV;
+	TextView titleTV;
+	TextView inventoriedTV;
+	TextView partsneededTV;
 	EditText et;
 	int itemCount;
 	int itemCount2;
@@ -25,6 +25,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        //set up new linear layout
         ll = new LinearLayout(this);
         ll.setOrientation(LinearLayout.VERTICAL);
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
@@ -36,16 +37,30 @@ public class MainActivity extends Activity {
         header.setBackgroundColor(Color.RED);
         header.setTextColor(Color.WHITE);
         ll.addView(header);
+        titleTV = new TextView(this);
         
-        firstTV = new TextView(this);
+        //set up section header
+        header = new TextView(this);
+        header.setText("Parts Inventory");
+        header.setBackgroundColor(Color.CYAN);
+        header.setTextColor(Color.WHITE);
+        ll.addView(header);
+        inventoriedTV = new TextView(this);
+        
+        
+        
+        
+        
+        
+        
         
         //Loop of parts from resources
         String [] parts = {getString(R.string.item1), getString(R.string.item2), getString(R.string.item3), getString(R.string.item4), getString(R.string.item5)};
         itemCount = parts.length;
         for(int i=0; i < itemCount; i++){
-        	firstTV.append((parts[i] + "\n"));
+        	titleTV.append((parts[i] + "\n"));
         }
-        ll.addView(firstTV);
+        ll.addView(titleTV);
         
         
         setContentView (ll);
