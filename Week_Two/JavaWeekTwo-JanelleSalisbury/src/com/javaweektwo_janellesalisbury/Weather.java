6f806a07bfcf073b74d2475d3758d0a5dbf43e17
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-10-29 19:40:53 -0400

    weather class, returning city, country, temp etc (not sure if it is correct)

---
+++ Week_Two/JavaWeekTwo-JanelleSalisbury/src/com/javaweektwo_janellesalisbury/Weather.java
@@ -0,0 +1,30 @@
+package com.javaweektwo_janellesalisbury;
+
+public class Weather {
+	
+	private String city;
+	private String country;
+	private String condition;
+	private String temp;
+	private String humidity;
+	
+public Weather(){}
+
+public String getCity() { return city;}
+public void setCity(String city) {this.city = city;}
+
+public String getCountry() {return country;}
+public void setCountry(String country) {this.country = country;}
+
+public String getCondition() {return condition;}
+public void setCondition(String condition) {this.condition = condition;}
+
+public String getTemp() {return temp;}
+public void getTemp(String temp) {this.temp = temp;}
+
+public String getHumidity() {return humidity;}
+public void getHumidity(String humidity) {this.humidity = humidity;}
+
+
+
+}
