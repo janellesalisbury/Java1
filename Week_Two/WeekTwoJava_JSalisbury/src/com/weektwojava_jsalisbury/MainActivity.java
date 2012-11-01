29e526070504146073a08e6c9a1dbaa383c43161
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-01 16:06:53 -0400

    working on spinner for state names

--- Week_Two/WeekTwoJava_JSalisbury/src/com/weektwojava_jsalisbury/MainActivity.java
+++ Week_Two/WeekTwoJava_JSalisbury/src/com/weektwojava_jsalisbury/MainActivity.java
@@ -44,16 +44,65 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				EditText name = (EditText) v.getTag();
+				EditText states = (EditText) v.getTag();
 				
-				//pulling enum data and logging it to LogCat (temporary until I can figure out something)
-		    	String al = States.ALABAMA.toString();
-		    	String ak = States.ALASKA.toString();
-		    	String ga = States.GEORGIA.toString();
-		    	String ia = States.IOWA.toString();
-		    	String nj = States.NEWJERSEY.toString();
-		    	String wy = States.WYOMING.toString();
-		    	String[] statesArray = {al, ak, ga, ia, nj, wy};
+				//Array list of states for drop down list
+		    	//TODO: use this to make combo box
+		    	String AL = States.ALABAMA.toString();
+		    	String AK = States.ALASKA.toString();
+		    	String AZ = States.ARIZONA.toString();
+		    	String CA = States.ARKANSAS.toString();
+		    	String CO = States.CALIFORNIA.toString();
+		    	String CT = States.COLORADO.toString();
+		    	String DE = States.DELAWARE.toString();
+		    	String FL = States.FLORIDA.toString();
+		    	String GA = States.GEORGIA.toString();
+		    	String HI = States.HAWAII.toString();
+		    	String ID = States.IDAHO.toString();
+		    	String IL = States.ILLINOIS.toString();
+		    	String IN = States.INDIANA.toString();
+		    	String IA = States.IOWA.toString();
+		    	String KS = States.KANSAS.toString();
+		    	String KY = States.KENTUCKY.toString();
+		    	String LA = States.LOUISIANA.toString();
+		    	String ME = States.MAINE.toString();
+		    	String MD = States.MARYLAND.toString();
+		    	String MA = States.MASSACHUCETTS.toString();
+		    	String MI = States.MICHIGAN.toString();
+		    	String MN = States.MINNESOTA.toString();
+		    	String MS = States.MISSISSIPPI.toString();
+		    	String MO = States.MISSOURI.toString();
+		    	String MT = States.MONTANA.toString();
+		    	String NE = States.NEBRASKA.toString();
+		    	String NV = States.NEVADA.toString();
+		    	String NH = States.NEW_HAMPSHIRE.toString();
+		    	String NJ = States.NEW_JERSEY.toString();
+		    	String NM = States.NEW_MEXICO.toString();
+		    	String NY = States.NEW_YORK.toString();
+		    	String NC = States.NORTH_CAROLINA.toString();
+		    	String ND = States.NORTH_DAKOTA.toString();
+		    	String OH = States.OHIO.toString();
+		    	String OK = States.OKLAHOMA.toString();
+		    	String OR = States.OREGON.toString();
+		    	String PA = States.PENNSYLVANIA.toString();
+		    	String RI = States.RHODE_ISLAND.toString();
+		    	String SC = States.SOUTH_CAROLINA.toString();
+		    	String SD = States.SOUTH_DAKOTA.toString();
+		    	String TN = States.TENNESSEE.toString();
+		    	String TX = States.TEXAS.toString();
+		    	String UT = States.UTAH.toString();
+		    	String VT = States.VERMONT.toString();
+		    	String VA = States.VIRGINIA.toString();
+		    	String WA = States.WASHINGTON.toString();
+		    	String WV = States.WEST_VIRGINIA.toString();
+		    	String WI = States.WISCONSIN.toString();
+		    	String WY = States.WYOMING.toString();
+		    	
+		    	String[] statesArray = {AL, AK, AZ, CA, CO, CT, CO, DE, FL, GA, HI, 
+		    							ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, 
+		    							MN, MS, MO, MT, NE, NV, NH, NJ, NH, NM, NY,
+		    							NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX,
+		    							UT, VT, VA, WA, WV, WI, WY};
 		    	
 		    	// compare data from enum and list out
 		    	if(States.ALASKA.toString() == "Alaska"){
@@ -82,8 +131,7 @@ public class MainActivity extends Activity {
     	stateWomenTV.setText(R.string.al_women);
     	
     	
-    	//Array list of states for drop down list
-    	//TODO: use this to make combo box
+    	//list of results from records that will be pulled from API and displayed on screen to user (next week)
     	ArrayList<StateInfo>records = new ArrayList<StateInfo>();
     	records.add(new CensusRecords("Alabama", 4802735, 51.5f));
     	records.add(new CensusRecords("Alaska", 710231, 48.1f));
