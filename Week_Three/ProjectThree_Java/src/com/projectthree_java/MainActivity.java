0ff86c914d46e745ae71e3ab176dce4eed9150e3
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-07 14:36:26 -0500

    yay! working

--- Week_Three/ProjectThree_Java/src/com/projectthree_java/MainActivity.java
+++ Week_Three/ProjectThree_Java/src/com/projectthree_java/MainActivity.java
@@ -1,14 +1,47 @@
 package com.projectthree_java;
 
+import com.salisbury.libs.SearchForm;
+
+import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+	
+	Context _context;
+	LinearLayout _appLayout;
+	SearchForm _search;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        _context = this;
+        _appLayout = new LinearLayout(this);
+        
+        _search = new SearchForm(_context, "Enter State", "Search Now");
+        
+        //ADD SEARCH HANDLER
+        EditText searchField = _search.getField();
+        Button searchButton = _search.getButton();
+        
+        searchButton.setOnClickListener(new OnClickListener(){
+        	
+        	public void onClick(View v){
+        		//GET STATE INFORMATION
+        		Log.i("Click Handler", _search.getField().getText().toString());
+        	}
+        });
+        
+        _appLayout.addView(_search);
+        setContentView(_appLayout);
         
     }
 
---
+++ Week_Three/ProjectThree_Java/src/com/salisbury/libs/StateDisplays.java
@@ -0,0 +1,5 @@
+package com.salisbury.libs;
+
+public class StateDisplays {
+
+}
