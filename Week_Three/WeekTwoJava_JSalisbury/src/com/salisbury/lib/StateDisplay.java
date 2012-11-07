b26795d1e11267711f91919fcb7d85fdc7b3254d
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-06 21:13:23 -0500

    attempting grid layout

---
+++ Week_Three/WeekTwoJava_JSalisbury/src/com/salisbury/lib/StateDisplay.java
@@ -0,0 +1,44 @@
+package com.salisbury.lib;
+
+import android.content.Context;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+public class StateDisplay extends GridLayout {
+	
+	TextView _name;
+	TextView _population;
+	TextView _percentWomen;
+	Context _context;
+	
+	public StateDisplay(Context context){
+		super (context);
+		
+		_context = _context;
+		
+		this.setColumnCount(2);
+		TextView nameLabel = new TextView(_context);
+		nameLabel.setText("State Name:");
+		_name = new TextView(_context);
+		
+		TextView populationLabel = new TextView(_context);
+		populationLabel.setText("Population:");
+		_population = new TextView(_context);
+		
+		TextView percentWomenLabel = new TextView(_context);
+		percentWomenLabel.setText("Percentage of Women");
+		_percentWomen = new TextView(_context);
+		
+		this.addView(nameLabel);
+		this.addView(_name);
+		this.addView(populationLabel);
+		this.addView(_population);
+		this.addView(percentWomenLabel);
+		this.addView(_percentWomen);
+		
+	}
+	
+	
+	
+
+}
