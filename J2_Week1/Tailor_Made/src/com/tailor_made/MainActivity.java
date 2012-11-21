14a2627cb371febabf89abd1def8fd37f15b07e0
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-21 01:13:52 -0500

    finally!!!

--- J2_Week1/Tailor_Made/src/com/tailor_made/MainActivity.java
+++ J2_Week1/Tailor_Made/src/com/tailor_made/MainActivity.java
@@ -1,21 +1,39 @@
 package com.tailor_made;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         
+        
+        //STORING STRING RESOURCES INTO ARRAY
+        String [] clothing_categories = getResources().getStringArray(R.array.clothing_categories);
+        
+        //BINDING TO LISTADAPTER
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, clothing_categories));
+        
+      
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	
 }
