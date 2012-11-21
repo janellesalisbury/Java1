a9af081d78ccd1db7c2e64b0afc376449010ba75
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-20 20:50:35 -0500

    on the right track, now just need an app idea

--- J2_Week1/MultiViewActivity/src/com/example/multiviewactivity/MainActivity.java
+++ J2_Week1/MultiViewActivity/src/com/example/multiviewactivity/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.multiviewactivity;
 
+import java.net.URI;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
 
@@ -30,11 +36,15 @@ public class MainActivity extends Activity {
 							
 			}
 		});
+        
+       
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        if(getIntent().getExtras() != null){
+        Log.i("IMAGE LOADED",getIntent().getExtras().get(Intent.EXTRA_STREAM).toString());
+        iv.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+        }
+        
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+   
 }
+
