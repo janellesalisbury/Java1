e22e59b55a8900833f4f0ceb45cb0d8fe0ca3997
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-22 20:43:38 -0500

    Finally got the webview working!

--- J2_Week1/Tailor_Made/src/com/tailor_made/WebView.java
+++ J2_Week1/Tailor_Made/src/com/tailor_made/WebView.java
@@ -1,7 +1,15 @@
 package com.tailor_made;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class WebView extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.web);
+		
+		
+	}
 
 }
