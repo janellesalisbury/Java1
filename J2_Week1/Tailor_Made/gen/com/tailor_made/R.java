f8579dd16b145487eedde1013a61365212788ac4
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-22 22:12:47 -0500

    ehhh, not pretty but it works and requirements are in

--- J2_Week1/Tailor_Made/gen/com/tailor_made/R.java
+++ J2_Week1/Tailor_Made/gen/com/tailor_made/R.java
@@ -16,6 +16,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int mall=0x7f020002;
+        public static final int sale=0x7f020003;
+        public static final int shopping=0x7f020004;
     }
     public static final class id {
         public static final int buttonUrl=0x7f080002;
