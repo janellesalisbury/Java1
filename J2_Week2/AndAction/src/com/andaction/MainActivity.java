528925b230e3a00ec73253d16b5668f86eeabc5c
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-26 13:26:53 -0500

    may have found the error.

--- J2_Week2/AndAction/src/com/andaction/MainActivity.java
+++ J2_Week2/AndAction/src/com/andaction/MainActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        
+        //STORING STRING RESOURCES INTO AN ARRAY
+        String [] actor_names= getResources().getStringArray(R.array.actor_names);
+        
+        //BINDING TO LIST ADAPTER
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, actor_names));
     }
 
     @Override
