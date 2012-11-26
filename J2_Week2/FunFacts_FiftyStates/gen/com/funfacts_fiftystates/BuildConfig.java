53ea96f410dfb996b8ea69a034374a413004aac4
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-25 20:33:07 -0500

    Better idea….

--- J2_Week2/FunFacts_FiftyStates/gen/com/funfacts_fiftystates/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.funfacts_fiftystates;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ J2_Week2/Lights_Camera_Action/gen/com/lights_camera_action/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lights_camera_action;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
