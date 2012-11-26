95dfc7af43838c79d453418d3a7d02493568feaa
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-26 12:30:15 -0500

    listview

--- J2_Week2/LightsCameraAction/src/com/lightscameraaction/MainActivity.java
+++ J2_Week2/LightsCameraAction/src/com/lightscameraaction/MainActivity.java
@@ -2,14 +2,21 @@ package com.lightscameraaction;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //STORING STRING RESOURCES INTO ARRAY
+        String [] actor_names = getResources().getStringArray(R.array.actor_names);
+        //BINDING TO LISTADAPTER
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, actor_names));
     }
 
     @Override
