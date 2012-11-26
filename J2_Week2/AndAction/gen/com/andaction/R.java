a130468571edd8356f64a8f464ecc617fe806262
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-26 15:52:45 -0500

    finally in and working

--- J2_Week2/AndAction/gen/com/andaction/R.java
+++ J2_Week2/AndAction/gen/com/andaction/R.java
@@ -20,13 +20,15 @@ public final class R {
     public static final class id {
         public static final int buttonUrl=0x7f080002;
         public static final int label=0x7f080000;
-        public static final int menu_settings=0x7f080003;
+        public static final int menu_settings=0x7f080004;
         public static final int name_label=0x7f080001;
+        public static final int webView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int list_item=0x7f030001;
         public static final int single_list_item_view=0x7f030002;
+        public static final int web=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- J2_Week2/AndAction/src/com/andaction/WebView.java
+++ J2_Week2/AndAction/src/com/andaction/WebView.java
@@ -1,5 +1,13 @@
 package com.andaction;
 
-public interface WebView {
 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WebView extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.web);
+		
+	}
 }
