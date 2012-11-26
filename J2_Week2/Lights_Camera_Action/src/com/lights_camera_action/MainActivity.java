405717b20f90e7ca930ccfe390e366279038a5a7
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-25 21:30:58 -0500

    in and working, up next…. adding images to listview and second activity using intent

--- J2_Week2/Lights_Camera_Action/src/com/lights_camera_action/MainActivity.java
+++ J2_Week2/Lights_Camera_Action/src/com/lights_camera_action/MainActivity.java
@@ -2,14 +2,18 @@ package com.lights_camera_action;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        setListAdapter (new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.actors)));
     }
 
     @Override
