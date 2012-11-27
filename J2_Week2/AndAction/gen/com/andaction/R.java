f089d7d36b24e0b53dfe59188bf3d753c78cc340
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-26 20:49:31 -0500

    oh boy

--- J2_Week2/AndAction/gen/com/andaction/R.java
+++ J2_Week2/AndAction/gen/com/andaction/R.java
@@ -14,8 +14,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int chevy=0x7f020000;
+        public static final int clint=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int jim=0x7f020004;
+        public static final int john=0x7f020005;
+        public static final int johnny=0x7f020006;
+        public static final int judy=0x7f020007;
+        public static final int marilyn=0x7f020008;
+        public static final int matthew=0x7f020009;
+        public static final int penelope=0x7f02000a;
     }
     public static final class id {
         public static final int buttonUrl=0x7f080002;
