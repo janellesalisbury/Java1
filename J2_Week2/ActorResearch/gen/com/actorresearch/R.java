6a56b58a57ea56aa2a1b8a4416c906143fea97e1
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-27 20:26:49 -0500

    maybe have figured out, but still getting error on setListAdapter

--- J2_Week2/ActorResearch/gen/com/actorresearch/R.java
+++ J2_Week2/ActorResearch/gen/com/actorresearch/R.java
@@ -10,7 +10,6 @@ package com.actorresearch;
 public final class R {
     public static final class array {
         public static final int actor_names=0x7f040000;
-        public static final int imdb_link=0x7f040001;
     }
     public static final class attr {
     }
@@ -22,7 +21,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button=0x7f090002;
+        public static final int button1=0x7f090002;
         public static final int editText=0x7f090001;
         public static final int mainListView=0x7f090003;
         public static final int menu_settings=0x7f090005;
--- J2_Week2/ActorResearch/src/com/actorresearch/MainActivity.java
+++ J2_Week2/ActorResearch/src/com/actorresearch/MainActivity.java
@@ -3,11 +3,16 @@ package com.actorresearch;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -15,6 +20,50 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //CREATE BUTTON/ONCLICK
+        Button doSearch = (Button) findViewById(R.id.button1);
+        doSearch.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				
+				ListView mainListView = (ListView) findViewById(R.layout.activity_main);
+			    mainListView.setListAdapter(ArrayAdapter.createFromResource(getApplicationContext(), 
+			    		R.array.actor_names, R.layout.activity_main));
+			    mainListView.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent, View v,
+							int position, long id) {
+						
+						//LAUNCH NEW ACTIVITY
+						Intent intent = new Intent(getApplicationContext(), WebView.class);
+						
+						//SEND TO NEW ACTIVITY
+						startActivity(intent);
+						
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+						
+						
+					}
+			    	
+			    });
+			      
+			    
+				
+				
+			}
+        	
+        });
+        
+       
+       
+       
+        
         
         
     }
