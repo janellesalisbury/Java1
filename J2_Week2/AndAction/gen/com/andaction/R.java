d6163496e1f9721b6e16192e36b4ec809a946965
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-11-29 19:27:30 -0500

    fragments are not great

--- J2_Week2/AndAction/gen/com/andaction/R.java
+++ J2_Week2/AndAction/gen/com/andaction/R.java
@@ -30,17 +30,21 @@ public final class R {
         public static final int penelope=0x7f02000a;
     }
     public static final class id {
-        public static final int button_url=0x7f090002;
+        public static final int button_url=0x7f090003;
         public static final int label=0x7f090000;
-        public static final int menu_settings=0x7f090004;
-        public static final int name_label=0x7f090001;
-        public static final int webView1=0x7f090003;
+        public static final int main_fragment=0x7f090001;
+        public static final int menu_settings=0x7f090006;
+        public static final int name_label=0x7f090002;
+        public static final int webView1=0x7f090004;
+        public static final int web_fragment=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int list_item=0x7f030001;
-        public static final int single_list_item_view=0x7f030002;
-        public static final int web=0x7f030003;
+        public static final int main_fragment=0x7f030002;
+        public static final int single_list_item_view=0x7f030003;
+        public static final int web=0x7f030004;
+        public static final int web_fragment=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
--- J2_Week2/AndAction/src/com/andaction/WebViewFragment.java
+++ J2_Week2/AndAction/src/com/andaction/WebViewFragment.java
@@ -1,7 +1,22 @@
 package com.andaction;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class WebViewFragment extends Fragment{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		WebView view = (WebView) inflater.inflate(R.layout.web, container, false);
+		
+		
+	}
 
 }
