a20ba76a29bdb651e1f0155511ebd2587a57bffa
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-12-04 13:36:55 -0500

    just trying to figure out this stuff

--- J2_Week2/JavaWeek2_FragmentProject/src/com/javaweek2_fragmentproject/MainActivityFragment.java
+++ J2_Week2/JavaWeek2_FragmentProject/src/com/javaweek2_fragmentproject/MainActivityFragment.java
@@ -1,7 +1,6 @@
 package com.javaweek2_fragmentproject;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
