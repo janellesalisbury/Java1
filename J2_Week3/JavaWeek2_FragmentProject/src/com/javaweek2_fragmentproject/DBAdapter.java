b78e07eb9f808f31b7f2535562ef7c4263a511c9
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-12-06 16:37:22 -0500

    creating a database for the content provider

---
+++ J2_Week3/JavaWeek2_FragmentProject/src/com/javaweek2_fragmentproject/DBAdapter.java
@@ -0,0 +1,53 @@
+package com.javaweek2_fragmentproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBAdapter {
+	public static final String KEY_ROWID= "_id";
+	public static final String KEY_TITLE= "title";
+	public static final String KEY_YEAR= "year";
+	public static final String TAG= "DBAdapter";
+	
+	private static final String DATABASE_NAME= "movies";
+	private static final String DATABASE_TABLE= "titles";
+	private static final int  DATABASE_VERSION= 1;
+	
+	private static final String DATABASE_CREATE= "create table titles (_id integer primary key autoincrement,"
+			+ "title tex not null," +"year text not null);";
+	
+	private final Context context; 
+	
+	private DatabaseHelper DBHelper;
+	private SQLiteDatabase db;
+	
+	public DBAdapter (Context ctx){
+		this.context = ctx;
+		DBHelper = new DatabaseHelper(context);
+	}
+	
+	private static class DatabaseHelper extends SQLiteOpenHelper 
+    {
+        DatabaseHelper(Context context) 
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+ 
+        @Override
+        public void onCreate(SQLiteDatabase db) 
+        {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version" + oldVersion + "to" + newVersion + "which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS titles");
+			onCreate(db);
+			
+		}
+ 
+    }
+}
