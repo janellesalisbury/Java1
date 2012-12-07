081685a53d09b9a254b9a2a60be7551db2d922fe
Janelle Salisbury <janellesalisbury@fullsail.edu>
2012-12-06 21:23:25 -0500

    I want to make the button go to the website…. using the service

--- J2_Week3/JavaWeek2_FragmentProject/gen/com/javaweek2_fragmentproject/R.java
+++ J2_Week3/JavaWeek2_FragmentProject/gen/com/javaweek2_fragmentproject/R.java
@@ -31,21 +31,17 @@ public final class R {
         public static final int penelope=0x7f02000b;
     }
     public static final class id {
-        public static final int actorName=0x7f080005;
-        public static final int actor_list_fragment=0x7f080007;
-        public static final int correctButton=0x7f080003;
-        public static final int image=0x7f080006;
-        public static final int image_viewer_fragment=0x7f080004;
-        public static final int menu_settings=0x7f080008;
-        public static final int noTasksButton=0x7f080001;
-        public static final int radioGroup=0x7f080000;
-        public static final int randomButton=0x7f080002;
+        public static final int actorName=0x7f080001;
+        public static final int actor_list_fragment=0x7f080004;
+        public static final int image=0x7f080003;
+        public static final int image_viewer_fragment=0x7f080000;
+        public static final int intentService=0x7f080002;
+        public static final int menu_settings=0x7f080005;
     }
     public static final class layout {
-        public static final int image_list=0x7f030000;
-        public static final int image_viewer_activity=0x7f030001;
-        public static final int image_viewer_fragment=0x7f030002;
-        public static final int main=0x7f030003;
+        public static final int image_viewer_activity=0x7f030000;
+        public static final int image_viewer_fragment=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- J2_Week3/JavaWeek2_FragmentProject/src/com/javaweek2_fragmentproject/ActorDetailsView.java
+++ J2_Week3/JavaWeek2_FragmentProject/src/com/javaweek2_fragmentproject/ActorDetailsView.java
@@ -3,9 +3,48 @@ package com.javaweek2_fragmentproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.view.View;
+import android.widget.Toast;
 
 public class ActorDetailsView extends Activity {
+	private Handler handler = new Handler() {
+	    public void handleMessage(Message message) {
+	      Object path = message.obj;
+	      if (message.arg1 == RESULT_OK && path != null) {
+	        Toast.makeText(ActorDetailsView.this,
+	            "Downloaded" + path.toString(), Toast.LENGTH_LONG)
+	            .show();
+	      } else {
+	        Toast.makeText(ActorDetailsView.this, "Download failed.",
+	            Toast.LENGTH_LONG).show();
+	      }
+
+	    };
+	  };
+//
+//	  @Override
+//	  public void onCreate(Bundle savedInstanceState) {
+//	    super.onCreate(savedInstanceState);
+//	    setContentView(R.layout.main);
+
+	  //}
+
+	  public void onClick(View view) {
+	    Intent intent = new Intent(this, DownloadService.class);
+	    // Create a new Messenger for the communication back
+	    Messenger messenger = new Messenger(handler);
+	    intent.putExtra("MESSENGER", messenger);
+	    intent.setData(Uri.parse("http://www.imdb.com"));
+	    intent.putExtra("urlpath", "http://www.imdb.com");
+	    startService(intent);
+	  }
+	
+
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
