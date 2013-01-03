4538bf0db76ebb84264bb5b4c201a574b411244b
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-01-02 20:54:32 -0500

    attempting to get going

--- 1301Java_Week1/RestaurantJunkie/src/com/restaurantjunkie/MainActivity.java
+++ 1301Java_Week1/RestaurantJunkie/src/com/restaurantjunkie/MainActivity.java
@@ -2,7 +2,10 @@ package com.restaurantjunkie;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,8 +13,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
+	// called when the user clicks the search button
+	public void sendSearch (View v){
+		Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+		//intent.putExtra("text_entry")
+		startActivity(intent);
+		
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
---
+++ 1301Java_Week1/RestaurantJunkie/src/com/restaurantjunkie/SecondActivity.java
@@ -0,0 +1,5 @@
+package com.restaurantjunkie;
+
+public class SecondActivity {
+
+}
