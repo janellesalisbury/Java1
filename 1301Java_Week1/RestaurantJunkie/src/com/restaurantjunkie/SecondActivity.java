06a1881ba0775b8b27ef8bdc7c8cf39a52c53ed3
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-01-06 20:52:01 -0500

    listview opens webview via implicit intent, still working on firming up the Uri

--- 1301Java_Week1/RestaurantJunkie/src/com/restaurantjunkie/SecondActivity.java
+++ 1301Java_Week1/RestaurantJunkie/src/com/restaurantjunkie/SecondActivity.java
@@ -1,11 +1,17 @@
 package com.restaurantjunkie;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SecondActivity extends ListActivity{
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -13,7 +19,28 @@ public class SecondActivity extends ListActivity{
 		
 		setListAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,
 				getResources().getStringArray(R.array.restaurants)));
+		
+		ListView lv = getListView();
+		
+		//OnClick
+		lv.setOnItemClickListener(new OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Intent internet = new Intent(Intent.ACTION_VIEW, Uri.parse("http://api.v3.factual.com/t/restaurants?key=gPkail5I7qZheLWqk085rXmWu68IXLw1lqGHEqIw"));
+				startActivity(internet);
+					
+				}
+				
+			});
 		
 	}
+
 }
+
+	
+
+	
+
+
