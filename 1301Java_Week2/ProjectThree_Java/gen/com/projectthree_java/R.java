c41df70bcbd73ce3e028640d5aed4391f85cc5b2
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-01-14 20:11:36 -0500

    XML layout is running correctly

--- 1301Java_Week2/ProjectThree_Java/gen/com/projectthree_java/R.java
+++ 1301Java_Week2/ProjectThree_Java/gen/com/projectthree_java/R.java
@@ -15,11 +15,16 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int black_pop=0x7f070006;
         public static final int bookmark_button=0x7f070002;
-        public static final int menu_settings=0x7f070004;
+        public static final int menu_settings=0x7f070009;
+        public static final int native_pop=0x7f070007;
+        public static final int other_pop=0x7f070008;
         public static final int search_button=0x7f070001;
         public static final int search_field=0x7f070000;
+        public static final int state_pop=0x7f070004;
         public static final int statedata=0x7f070003;
+        public static final int white_pop=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -30,11 +35,16 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int bkmk_it=0x7f040006;
+        public static final int black_=0x7f040009;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int native_american_=0x7f04000a;
+        public static final int other_=0x7f04000b;
         public static final int please_enter_a_state=0x7f040004;
         public static final int search=0x7f040005;
+        public static final int state_population_=0x7f040007;
         public static final int title_activity_main=0x7f040003;
+        public static final int white_=0x7f040008;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
