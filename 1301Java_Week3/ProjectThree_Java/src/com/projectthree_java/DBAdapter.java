70391b9ca116b08f9df516b9c392de910ed5b17b
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-01-23 12:23:38 -0500

    Creating content provider database

---
+++ 1301Java_Week3/ProjectThree_Java/src/com/projectthree_java/DBAdapter.java
@@ -0,0 +1,15 @@
+package com.projectthree_java;
+
+public class DBAdapter {
+	
+	public static final String KEY_ROWID= "_id";
+	public static final String KEY_NAME= "name";
+	public static final String KEY_POPULATION= "population";
+	
+	private static final String TAG= "DBAdapter";
+	private static final String DATABASE_NAME= "states";
+	private static final String DATABASE_TABLE= "population";
+	private static final int DATABASE_VERSION= 1;
+	
+
+}
