6b279bcf5a86a2b957538d038aa1742ce47d0632
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-06 21:29:43 -0500

    OK FIXED IT

--- WeekOne_ImplicitIntent/src/com/weekone_implicitintent/MainActivity.java
+++ WeekOne_ImplicitIntent/src/com/weekone_implicitintent/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//THIS IS A TEST TO SEE IF GITHUB IS WORKING!
 	}
 
 	@Override
