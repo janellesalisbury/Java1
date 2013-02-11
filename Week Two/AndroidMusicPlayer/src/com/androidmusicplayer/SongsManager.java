28d366633cc94535c380aa2b840d37d301e0f739
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-11 12:56:37 -0500

    creating java code to play songs and handle audio control

---
+++ Week Two/AndroidMusicPlayer/src/com/androidmusicplayer/SongsManager.java
@@ -0,0 +1,16 @@
+package com.androidmusicplayer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SongsManager {
+	//SD CARD PATH
+	final String MEDIA_PATH = new String("/sdcard/");
+	private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	
+	//CONSTRUCTOR
+	public SongsManager(){
+		
+	}
+
+}
