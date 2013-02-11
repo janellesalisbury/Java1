1aabc4f6ab5e2802236a320088122bbdfc7b6262
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-11 13:22:11 -0500

    Completed song handler class, added mp3 to sdcard

--- Week Two/AndroidMusicPlayer/src/com/androidmusicplayer/SongsManager.java
+++ Week Two/AndroidMusicPlayer/src/com/androidmusicplayer/SongsManager.java
@@ -1,5 +1,7 @@
 package com.androidmusicplayer;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,5 +14,31 @@ public class SongsManager {
 	public SongsManager(){
 		
 	}
+	
+	//READ FILES AND STORE TO LIST
+	public ArrayList<HashMap<String, String>> getPlaylist(){
+		File home = new File(MEDIA_PATH);
+		
+		if (home.listFiles(new FileExtensionFilter()).length > 0){
+			for (File file : home.listFiles(new FileExtensionFilter())){
+				HashMap<String, String> song = new HashMap<String, String>();
+				song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
+				song.put("songPath", file.getPath());
+				
+				//ADD THE SONG TO THE LIST
+				songsList.add(song);
+			}
+		}
+		//RETURN SONGS ARRAY
+		return songsList;
+				
+	}
+	//FILTER MP3 FILES
+	class FileExtensionFilter implements FilenameFilter{
+		public boolean accept(File dir, String name) {
+            return (name.endsWith(".mp3") || name.endsWith(".MP3"));
+
+	}
+}
 
 }
