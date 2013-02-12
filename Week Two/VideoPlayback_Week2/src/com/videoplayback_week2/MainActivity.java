f506d8ef136f6f9e22d8e6fe04949dccb951df81
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-11 20:09:29 -0500

    maybe this will sync now

--- Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
+++ Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
@@ -3,6 +3,7 @@ package com.videoplayback_week2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		VideoView v = (VideoView) findViewById(R.id.videoView1);
+		v.setVideoPath("/sdcard/Moves Like Jagger.mp4");
 	}
 
 	@Override
