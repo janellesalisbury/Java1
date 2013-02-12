83ca89b3a09763ee4e3725ff497d36e02b5e74a7
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-11 21:09:51 -0500

    woo hoo I got video working!!

--- Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
+++ Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
@@ -1,15 +1,48 @@
 package com.videoplayback_week2;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
+	//GLOBAL VARIABLES
+	String buttonText;
+	Button button1;
+	VideoView vv;
+	Context _this;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//SET CONTEXT
+		_this = this;
+		
+		//SET UP BUTTON FOR VIDEO PLAYBACK
+		Button button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//PLAY VIDEO NOW
+				vv = (VideoView) findViewById(R.id.videoView1);
+				String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.ironman;
+				vv.setVideoURI(Uri.parse(uriPath));
+				vv.setMediaController(new MediaController(_this));
+				vv.start();
+				
+				
+			}
+		});
 
 	}
 
