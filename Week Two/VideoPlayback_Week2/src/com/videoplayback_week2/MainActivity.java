0ef83a69abc12b0c3d73c5ed203a7a6df7d2dd19
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-02-12 13:49:27 -0500

    Netowrk connectivity in, just need to have it handle changes now

--- Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
+++ Week Two/VideoPlayback_Week2/src/com/videoplayback_week2/MainActivity.java
@@ -3,7 +3,9 @@ package com.videoplayback_week2;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,18 +17,74 @@ public class MainActivity extends Activity {
 	//GLOBAL VARIABLES
 	String buttonText;
 	Button button1;
+	Button button2;
 	VideoView vv;
 	Context _this;
+	
+	//INTERNET CONNECTED BOOLEAN
+    Boolean isInternetPresent = false;
+ 
+    // CONNECTION DETECTOR
+    NetworkDetection cd;
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+	        
 		//SET CONTEXT
 		_this = this;
 		
+		Button btnStatus = (Button) findViewById(R.id.button2);
+		 
+        // creating connection detector class instance
+        cd = new NetworkDetection(getApplicationContext());
+        btnStatus.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View v) {
+ 
+                // get Internet status
+                isInternetPresent = cd.isConnectingToInternet();
+ 
+                // check for Internet status
+                if (isInternetPresent) {
+                    // Internet Connection is Present
+                    // make HTTP requests
+                    showAlertDialog(MainActivity.this, "Internet Connection",
+                            "You're connected", true);
+                } else {
+                    // Internet connection is not present
+                    // Ask user to connect to Internet
+                    showAlertDialog(MainActivity.this, "No Internet Connection",
+                            "Oops, please select a network", false);
+                }
+            }
+ 
+        });
+ 
+    }
+ 
+    @SuppressWarnings("deprecation")
+	public void showAlertDialog(Context context, String title, String message, Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+ 
+        //SETTING DIALOG TITLE
+        alertDialog.setTitle(title);
+ 
+        //SETTING DIALOG MESSAGE
+        alertDialog.setMessage(message);
+ 
+        //OK BUTTON
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+ 
+        //ALERT MESSAGE
+        alertDialog.show();
+    
 		//SET UP BUTTON FOR VIDEO PLAYBACK
 		Button button1 = (Button) findViewById(R.id.button1);
 		button1.setOnClickListener(new OnClickListener() {
