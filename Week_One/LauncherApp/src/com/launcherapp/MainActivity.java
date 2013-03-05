5b5f9bd4950530433458c3b6d7774e4305f1521f
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-05 14:00:19 -0500

    Launcher activity working, not passing data yet

--- Week_One/LauncherApp/src/com/launcherapp/MainActivity.java
+++ Week_One/LauncherApp/src/com/launcherapp/MainActivity.java
@@ -12,14 +12,15 @@ import android.widget.ListView;
 public class MainActivity extends ListActivity {
 	static final String[] Songs = new String[] {"Song 1", "Song 2", "Song 3", "Song 4",
 		"Song 5", "Song 6", "Song 7"};
+	String song;
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//CREATE LIST ADAPTER 
 		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, Songs));
-		
+		//GET LISTVIEW
 		ListView songsList = getListView();
 		songsList.setTextFilterEnabled(true);
 		
@@ -28,7 +29,10 @@ public class MainActivity extends ListActivity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int pos,
 					long id) {
+				//CLICK BUTTON AND OPEN MUSIC PLAYER IN SECOND APP
 				Intent MusicLauncher = getPackageManager().getLaunchIntentForPackage("com.musicplayer_mainapp");
+				//Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				//MusicLauncher.setData(Uri.parse(song));
 				startActivity(MusicLauncher);
 				
 			}
