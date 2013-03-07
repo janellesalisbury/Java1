60b426c98dcb3f41a05c16f717c731f4436b37a3
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-06 19:02:29 -0500

    Creating media player. Lets hope this one works

--- Week_One/MediaPlayer_MainApp/gen/com/mediaplayer_mainapp/R.java
+++ Week_One/MediaPlayer_MainApp/gen/com/mediaplayer_mainapp/R.java
@@ -17,16 +17,12 @@ public final class R {
         public static final int previous=0x7f020003;
     }
     public static final class id {
-        public static final int bg=0x7f070001;
-        public static final int btnNext=0x7f070005;
-        public static final int btnPlay=0x7f070004;
-        public static final int btnPrevious=0x7f070003;
-        public static final int buttons=0x7f070002;
-        public static final int menu_settings=0x7f070006;
-        public static final int screen=0x7f070000;
+        public static final int menu_settings=0x7f070001;
+        public static final int text1=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int song_item=0x7f030000;
+        public static final int songlist=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- Week_One/MediaPlayer_MainApp/src/com/mediaplayer_mainapp/MainActivity.java
+++ Week_One/MediaPlayer_MainApp/src/com/mediaplayer_mainapp/MainActivity.java
@@ -1,13 +1,53 @@
+/*
+ * project MediaPlayer_MainApp
+ * package com.mediaplayer_mainapp
+ * @author Janelle Salisbury
+ * date Mar 6, 2013
+ */
 package com.mediaplayer_mainapp;
 
-import android.os.Bundle;
-import android.app.Activity;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainActivity will hold the android media player and play the users sd card media files.
+ */
+public class MainActivity extends ListActivity {
+	//GLOBAL VARIABLES
+	private static final String MEDIA_PATH = new String("/sdcard/");
+	private List<String> songs = new ArrayList<String>();
+	private MediaPlayer mp = new MediaPlayer();
+	private int currentPostion = 0;
 	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.songlist);
+		updateSongList();
+		
+	}
+	//UPDATE SONGS FUNCTIONALITY
+	private void updateSongList() {
+		//create file object (home) which points to the sd card path
+		File home = new File(MEDIA_PATH);
+		if(home.listFiles(new Mp3Filter()).length > 0){
+			for(File file:home.listFiles(new Mp3Filter())){
+				songs.add(file.getName());
+			}
+			//create array adapter for song list
+			ArrayAdapter<String> songList = new ArrayAdapter<String> (this, R.layout.song_item, songs);
+			setListAdapter(songList);
+		}
 		
 	}
 }
