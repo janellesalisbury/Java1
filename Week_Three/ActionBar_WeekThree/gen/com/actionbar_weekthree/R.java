1d140ad7d549b098763ff24b4e2f9c356cabdd00
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-18 16:44:23 -0400

    adding icons for the action bar functionality

--- Week_Three/ActionBar_WeekThree/gen/com/actionbar_weekthree/R.java
+++ Week_Three/ActionBar_WeekThree/gen/com/actionbar_weekthree/R.java
@@ -11,9 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_action_share=0x7f020002;
+        public static final int ic_action_star=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
+        public static final int action_refresh=0x7f070001;
+        public static final int action_search=0x7f070002;
+        public static final int action_share=0x7f070003;
+        public static final int action_user=0x7f070004;
         public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
@@ -21,6 +29,7 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
+        public static final int mainmenu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
