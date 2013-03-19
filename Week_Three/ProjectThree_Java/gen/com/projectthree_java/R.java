bc7ca1d0c6dc131b74451bd1b2c73cd14edcffef
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-19 19:31:27 -0400

    Second View in and working, needs to display different info though

--- Week_Three/ProjectThree_Java/gen/com/projectthree_java/R.java
+++ Week_Three/ProjectThree_Java/gen/com/projectthree_java/R.java
@@ -18,14 +18,13 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_refresh=0x7f070004;
-        public static final int action_search=0x7f070005;
-        public static final int action_share=0x7f070006;
-        public static final int action_user=0x7f070007;
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070003;
+        public static final int action_refresh=0x7f070003;
+        public static final int action_search=0x7f070004;
+        public static final int action_share=0x7f070005;
+        public static final int action_user=0x7f070006;
+        public static final int menu_settings=0x7f070002;
         public static final int moreDetails=0x7f070000;
-        public static final int webView1=0x7f070002;
+        public static final int webView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
