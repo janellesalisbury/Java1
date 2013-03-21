d8eec001282c53ba3cfd1743cbeca524ca1f768c
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-21 16:55:12 -0400

    project files for widget

--- Week_Three/WidgetWeek3/gen/com/widgetweek3/BuildConfig.java
+++ Week_Three/WidgetWeek3/gen/com/widgetweek3/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.widgetportion;
+package com.widgetweek3;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
