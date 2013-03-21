6645a20b03c836aa70d721b2fc84cd057452b80a
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-21 18:27:13 -0400

    finished. To sick to stay up any longer

--- Week_Three/TimeWidget/src/com/timewidget/AppWidget.java
+++ Week_Three/TimeWidget/src/com/timewidget/AppWidget.java
@@ -1,3 +1,9 @@
+/*
+ * project TimeWidget
+ * package com.timewidget
+ * @author Janelle Salisbury
+ * date Mar 21, 2013
+ */
 package com.timewidget;
 
 import java.text.DateFormat;
@@ -13,8 +19,16 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AppWidget.
+ */
 public class AppWidget extends AppWidgetProvider{
 	
+	/* (non-Javadoc)
+	 * @see android.appwidget.AppWidgetProvider#onUpdate(android.content.Context, android.appwidget.AppWidgetManager, int[])
+	 */
+	//UPDATE WITH THE CUTTENT TIME EVERY SECOND
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		
@@ -22,18 +36,33 @@ public class AppWidget extends AppWidgetProvider{
 		time.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 1000);
 	}
 	
+	/**
+	 * The Class MyTime.
+	 */
+	
+	//CREATE THE WIDGET USING REMOTE VIEWS
 	private class MyTime extends TimerTask {
 		RemoteViews rv;
 		AppWidgetManager awm;
 		ComponentName myWidget;
 		DateFormat df = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
 		
+	/**
+	 * Instantiates a new my time.
+	 *
+	 * @param context the context
+	 * @param appWidgetManager the app widget manager
+	 */
 	public MyTime(Context context, AppWidgetManager appWidgetManager) {
 		this.awm = appWidgetManager;
 		rv = new RemoteViews(context.getPackageName(), R.layout.main);
 		myWidget = new ComponentName(context, AppWidget.class);
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.util.TimerTask#run()
+	 */
+	//RUN THE WIDGET AND UPDATE IT
 	@Override
 	public void run() {
 		rv.setTextViewText(R.id.widget_textview, "TIME = " +df.format(new Date()));
