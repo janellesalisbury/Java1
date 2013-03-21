92460a2512b5af93013d188a42923888dd6ce6d2
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-21 11:51:59 -0400

    creating xml layout for clock widget

--- Week_Three/AppWidget/gen/com/appwidget/R.java
+++ Week_Three/AppWidget/gen/com/appwidget/R.java
@@ -12,9 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int widget_bg_normal=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070001;
+        public static final int widget_textview=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,8 +26,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int widget_text=0x7f040001;
     }
     public static final class style {
         /** 
