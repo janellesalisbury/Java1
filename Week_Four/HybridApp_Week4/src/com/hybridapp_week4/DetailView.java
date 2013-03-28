fb967f0d535fe6508a2dded3115bcf7810e73b73
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-28 16:15:04 -0400

    write to external underway

--- Week_Four/HybridApp_Week4/src/com/hybridapp_week4/DetailView.java
+++ Week_Four/HybridApp_Week4/src/com/hybridapp_week4/DetailView.java
@@ -4,10 +4,12 @@ import java.io.File;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailView extends Activity{
 
@@ -25,7 +27,7 @@ public class DetailView extends Activity{
 		tv2.setText(getIntent().getExtras().getString("TaskLoc"));
 		
 		//CREATE THE FILE TO SAVE
-		File data = getFilesDir();
+		File data = getExternalFilesDir(null);
 		String path = data.getAbsolutePath();
 		
 		Button save = (Button) findViewById(R.id.button1);
@@ -34,10 +36,27 @@ public class DetailView extends Activity{
 			@Override
 			public void onClick(View v) {
 				
+
+				
 				
 			}
 		});
 		
+		
+	}
+	
+	//CHECK FOR EXTERNAL STORAGE
+	public boolean isStorageAvailable(){
+		String available = Environment.getExternalStorageState();
+		if(available.equals(Environment.MEDIA_MOUNTED)){
+			return true;
+		}else if(available.equals(Environment.MEDIA_MOUNTED_READ_ONLY)){
+			Toast.makeText(this, "Read Only", Toast.LENGTH_SHORT).show();
+		}else{
+			Toast.makeText(this, "No Storage Available", Toast.LENGTH_SHORT).show();
+			
+		}
+		return false;
 		
 	}
 
