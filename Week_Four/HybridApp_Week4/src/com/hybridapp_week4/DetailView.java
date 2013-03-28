ad2bf260d3304f0306d98177862637aa8d14b7c9
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-03-28 13:46:17 -0400

    adding in storage of the data

--- Week_Four/HybridApp_Week4/src/com/hybridapp_week4/DetailView.java
+++ Week_Four/HybridApp_Week4/src/com/hybridapp_week4/DetailView.java
@@ -1,7 +1,12 @@
 package com.hybridapp_week4;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailView extends Activity{
@@ -12,13 +17,26 @@ public class DetailView extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_view);
 		
+		//CREATE TEXTVIEWS AND SET THE PASSED DATA
 		TextView tv = (TextView) findViewById(R.id.task);
 		tv.setText(getIntent().getExtras().getString("TaskName"));
 		
 		TextView tv2 = (TextView) findViewById(R.id.locale);
 		tv2.setText(getIntent().getExtras().getString("TaskLoc"));
 		
+		//CREATE THE FILE TO SAVE
+		File data = getFilesDir();
+		String path = data.getAbsolutePath();
 		
+		Button save = (Button) findViewById(R.id.button1);
+		save.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+		});
 		
 		
 	}
